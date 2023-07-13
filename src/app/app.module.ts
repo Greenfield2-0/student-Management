4ced4fab6127a87693c35f5b471ae94743f0a734
SabrineGc <gaceursabrine@yahoo.com>
2023-07-13 14:26:20 +0200

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentHomeComponent } from './student/student-home/student-home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './student/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentHomeComponent,
+    WelcomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
